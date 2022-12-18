23a19a09bcf52e06ef11d0593baa180374871578
krishnaanand2908 <krishnaanand290809@gmail.com>
2022-12-18 15:39:31 +0530

    hi

---
+++ Code-With-Harry-Tuts/File Reading.py
@@ -0,0 +1,1 @@
+open('panther.txt')
---
+++ file io basics.py
@@ -0,0 +1,11 @@
+# File IO Basics
+
+""" 
+r - Open file for reading
+w - Open file for writing
+x - Creates a new file fails if it exists
+a - Adds content to the file
+t - text mode. Data is text. Can be used to deal with strings.
+b - Binary Mode
++ - Read and Write
+"""
